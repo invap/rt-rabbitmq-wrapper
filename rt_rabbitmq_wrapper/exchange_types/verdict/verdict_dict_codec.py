913fcbc7852c2bfa2ca0ba350503f4137ab93731
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2025-08-19 14:17:01 -0300

    Corrected an erroneous import.

--- rt_rabbitmq_wrapper/exchange_types/verdict/verdict_dict_codec.py
+++ rt_rabbitmq_wrapper/exchange_types/verdict/verdict_dict_codec.py
@@ -6,7 +6,7 @@ import logging
 # Create a logger for the RabbitMQ utility component
 logger = logging.getLogger(__name__)
 
-from attic.verdict_codec_errors import (
+from verdict_codec_errors import (
     VerdictTypeError,
     VerdictDictError
 )
